676c423b7c470b15526bc7339dd0c515b7dd203f
findolor <anakisci@gmail.com>
2022-11-01 12:49:11 +0300

    Add deploy config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Komple Documentation',
   tagline: 'Documentation for Komple',
   url: 'https://docs.komple.io',
-  baseUrl: '/',
+  baseUrl: '/komple-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KompleTeam', // Usually your GitHub org/user name.
   projectName: 'komple-docs', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
